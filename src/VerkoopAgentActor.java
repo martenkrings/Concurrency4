5d709ca02d85ecac88a7eec6964d24348f37b236
Marten <krings.marten@gmail.com>
2016-10-11 12:41:42 +0200

    Initial commit

---
+++ src/VerkoopAgentActor.java
@@ -0,0 +1,11 @@
+import akka.actor.UntypedActor;
+
+/**
+ * Created by Marten on 10/11/2016.
+ */
+public class VerkoopAgentActor extends UntypedActor {
+    @Override
+    public void onReceive(Object o) throws Throwable {
+
+    }
+}
